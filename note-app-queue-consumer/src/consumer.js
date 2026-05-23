672e57ad609722123cf20f5f8e9f3d59c80ea818
Ihsanul Dzaky <ihsanuldzky971@gmail.com>
2026-05-23 14:22:40 +0700

    consumer program finished

---
+++ note-app-queue-consumer/src/consumer.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import amqp from 'amqplib';
+import NotesService from './NotesServices.js';
+import MailSender from './MailSender.js';
+import Listener from './Listener.js';
+const init = async () => {
+  const notesService = new NotesService();
+  const mailSender = new MailSender();
+  const listener = new Listener(notesService, mailSender);
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+  const channel = await connection.createChannel();
+  await channel.assertQueue('export:notes', {
+    durable: true,
+  });
+  channel.consume('export:notes', listener.listen, { noAck: true });
+};
+init();
